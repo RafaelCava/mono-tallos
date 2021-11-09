c544872d561fc335469f7adc0cc2e10b95eb6b16
rafael Home <ra.facavalcante@hotmail.com>
2021-11-09 19:44:03 -0300

    edit: Alterando components groups and Chat

--- front_tallos/src/components/Chat/Chat.tsx
+++ front_tallos/src/components/Chat/Chat.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, {
+  MutableRefObject, useEffect, useMemo, useRef,
+} from 'react';
+import io from 'socket.io-client';
 
 type Props = {
   id: string;
   name: string;
 }
 
-const Chat = (props: Props) => (
-  <div>
-    <h1>
-      Chat
-      {' '}
-      {props.name}
-      {props.id}
-    </h1>
-  </div>
-);
+const Chat = (props: Props) => {
+  const socket = useMemo<any>(() => io('http://localhost:3000'), []);
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('abriu sessão');
+    });
+  }, [socket]);
+
+  const inpuRef = useRef() as MutableRefObject<any>;
+
+  return (
+    <div>
+      <h1>
+        Chat
+      </h1>
+      <ul>
+        <li />
+      </ul>
+      <p>
+        <label htmlFor="message">Message:</label>
+        <input type="text" id="message" ref={inpuRef} />
+        <button type="button">Enviar</button>
+      </p>
+    </div>
+  );
+};
 
 export default Chat;
