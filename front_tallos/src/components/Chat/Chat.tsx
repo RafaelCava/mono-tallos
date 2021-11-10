148d1481998d251de0eace08b08f550e3595c1dc
rafael Home <ra.facavalcante@hotmail.com>
2021-11-10 00:59:29 -0300

    src: finalizando configurações

--- front_tallos/src/components/Chat/Chat.tsx
+++ front_tallos/src/components/Chat/Chat.tsx
@@ -1,23 +1,42 @@
 import React, {
-  MutableRefObject, useEffect, useMemo, useRef,
+  MutableRefObject, useEffect, useMemo, useRef, useState,
 } from 'react';
+import { useLocation } from 'react-router-dom';
 import io from 'socket.io-client';
-import { PropsChat } from '../../interfaces/interfaces';
+import { Message, PropsChat } from '../../interfaces/interfaces';
 
 const Chat = (props: PropsChat) => {
-  const socket = useMemo(() => io('http://localhost:3000', { transports: ['websocket'] }), []);
+  const [messages, setMessages] = useState<Message[]>([]);
+  const location = useLocation();
+
+  const queryParams: any = useMemo(() => {
+    const query = new URLSearchParams(location.search);
+    return {
+      group_id: query.get('group_id'),
+      name: query.get('name'),
+    };
+  }, [location]);
+
+  const socket = useMemo(() => io('http://localhost:3000', { transports: ['websocket'], query: { name: queryParams.name, group_id: queryParams.group_id } }), [queryParams]);
 
   useEffect(() => {
     socket.on('connect', () => {
       console.log('abriu a sessão');
 
-      socket.on('receive-message', (data: { message: string }) => console.log(data));
+      socket.on('receive-message', (data: { message: string, name: string }) => {
+        console.log(data);
+        setMessages((prevState) => [...prevState, data]);
+      });
     });
   }, [socket]);
 
   const sendMessage = () => {
     const message = inpuRef.current.value;
+    inpuRef.current.value = '';
     socket.emit('send-message', { message });
+    const { name } = queryParams;
+    const data = { message, name };
+    setMessages((prevState) => [...prevState, data]);
   };
 
   const inpuRef = useRef() as MutableRefObject<any>;
@@ -26,9 +45,19 @@ const Chat = (props: PropsChat) => {
     <div>
       <h1>
         Chat
+        {' '}
+        {queryParams.name}
       </h1>
       <ul>
-        <li />
+        {messages.map((message, index) => (
+          <li key={index}>
+            {message.name}
+            {' '}
+            -
+            {' '}
+            {message.message}
+          </li>
+        ))}
       </ul>
       <p>
         <label htmlFor="message">Message:</label>
--- front_tallos/src/interfaces/interfaces.ts
+++ front_tallos/src/interfaces/interfaces.ts
@@ -7,3 +7,8 @@ export interface GroupsModel{
   id: string
   name: string
 }
+
+export interface Message {
+  name: string
+  message: string
+}
