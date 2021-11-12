c73a4f318610637dea0d316959a11b61c344ed8d
rafael tallos <ra.facavalcante@hotmail.com>
2021-11-12 14:32:51 -0300

    add: dependencies

--- front_tallos/src/pages/Login/index.tsx
+++ front_tallos/src/pages/Login/index.tsx
@@ -1,17 +1,36 @@
-import * as React from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/button-has-type */
+import React, { useState } from 'react';
 
-const Login = () => (
-  <div className="container">
-    <div className="box">
-      <form action="" className="login">
-        <label htmlFor=""></label>
-        <input type="text" />
-        <input type="password" />
-        <a href="">criar conta</a>
-        <button>Login</button>
-      </form>
+const Login = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [formActive, setFormActive] = useState(true);
+  const handleTokenLocalStorage = (): void => {
+
+  };
+  const handleFormlogin = (): void => {
+    setFormActive(false);
+  };
+  return (
+    <div className="container">
+      <div className="box">
+        { formActive
+          ? (
+            <form action="" className="login">
+              <label htmlFor="emailLogin" />
+              <input onChange={(e) => setInputValue(e.target.value)} type="text" name="emailLogin" />
+              <input type="password" />
+              <a onClick={handleFormlogin}>criar conta</a>
+              <button onClick={handleTokenLocalStorage}>Login</button>
+            </form>
+          )
+          : (
+            <form action="" className="cadastrar" />
+          )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Login;
