8a602df401ed285a0ef7cc3e4ff69c599199db8f
rafael Home <ra.facavalcante@hotmail.com>
2021-11-14 14:16:47 -0300

    estilizando componente formulario de cadastro

--- front_tallos/src/components/FormCadastrar/index.tsx
+++ front_tallos/src/components/FormCadastrar/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import useHooks from '../../hooks/useHooks';
+import '../FormLogin/styles.css';
 
 const FormCadastrar = () => {
   const {
@@ -13,13 +14,13 @@ const FormCadastrar = () => {
     inputValueCadastrarSenha,
   } = useHooks();
   return (
-    <form className="box cadastrar">
+    <form>
       <h1>Cadastro</h1>
       <input type="text" placeholder="Insira seu Nome:" onChange={(e) => setInputValueCadastrarName(e.target.value)} value={inputValueCadastrarName} />
       <input type="email" placeholder="Insira seu E-mail:" onChange={(e) => setInputValueCadastrarEmail(e.target.value)} value={inputValueCadastrarEmail} />
       <input type="password" placeholder="Insira sua Senha:" onChange={(e) => setInputValueCadastrarSenha(e.target.value)} value={inputValueCadastrarSenha} />
-      <a onClick={handleFormSetLogin}>Voltar</a>
       <button onClick={(e) => cleanInput(e)}>Cadastrar</button>
+      <a onClick={handleFormSetLogin}>Voltar</a>
     </form>
   );
 };
