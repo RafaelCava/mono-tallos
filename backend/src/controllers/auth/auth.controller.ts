478ab4c089bc508c54697cff91a08ec3dd43729e
rafael Home <ra.facavalcante@hotmail.com>
2021-11-15 20:23:15 -0300

    backend/src/controllers/auth/auth.controller: Alterando controler

--- backend/src/controllers/auth/auth.controller.ts
+++ backend/src/controllers/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Response } from '@nestjs/common';
+import { Response as ResponseExpress } from 'express';
 import { AuthServices } from 'src/services/authServices/auth.service';
 
 @Controller('login')
@@ -6,7 +7,7 @@ export class AuthController {
   constructor(private authService: AuthServices) {}
 
   @Post()
-  async login(@Body() body): Promise<void> {
-    return await this.authService.verifyAuthorization(body);
+  async login(@Body() body, @Response() res: ResponseExpress): Promise<void> {
+    return await this.authService.verifyAuthorization(body, res);
   }
 }
